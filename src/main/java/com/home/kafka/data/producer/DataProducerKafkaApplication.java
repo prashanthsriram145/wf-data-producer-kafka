2ce98263bf46c66b6498bab800d74bf79a620125
Prashanth Sriram <prashanthsriram145@gmail.com>
2022-09-24 15:55:38 +0530

    intial commit

---
+++ src/main/java/com/home/kafka/data/producer/DataProducerKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.home.kafka.data.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataProducerKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataProducerKafkaApplication.class, args);
+	}
+
+}
